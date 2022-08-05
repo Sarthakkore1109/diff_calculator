66f98f6146372524975f555f1f237e34c2b799ec
scottmreed <53488644+scottmreed@users.noreply.github.com>
2022-08-05 10:56:24 -0600

    mac compatible

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def read_csv(filename):
     #pathTest = os.path.dirname(__file__)
     #latest_edited_file = max([f for f in os.scandir(os.path.join(pathTest, "diffs", "Spring 2022"))], key=lambda x: x.stat().st_mtime).name
     #print(latest_edited_file)
-    latest_edited_file_glob('test')
+    latest_edited_file_glob('test') # to get date
 
     path = os.path.dirname(__file__)
     print('File name :    ', os.path.basename(__file__))
