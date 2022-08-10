ce0326c2ca6122805afedb5730a9e93010524649
scottmreed <53488644+scottmreed@users.noreply.github.com>
2022-08-10 12:15:10 -0600

    ir ori empty, compares to most recent color_diff

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from openpyxl import Workbook
 import pandas as pd
 import os
@@ -17,23 +16,29 @@ def format_number(num):
         dec = decimal.Decimal(num)
     except:
         return num
-
     val = dec
     return val
 
-def latest_edited_file_glob(pathToDir):
+def latest_edited_file_glob(pathToDir, **kwargs):
     pathTest = os.path.dirname(__file__)
 
     if pathToDir == 'ori':
-        list_of_files = glob.glob(
-            os.path.join(pathTest, "data", "ori", '*'))  # * means all if need specific format then *.csv
-        latest_file = max(list_of_files, key=os.path.getctime)
-        print("glob latest_file:" + latest_file)
+        ori_file_path = os.path.join(pathTest, 'data', 'ori', 'CLASSCHED.csv')
+        if not os.path.isfile(ori_file_path):
+            print('ori is empty. assume you want to compare to most recent colored diff')
+            latest_file = 'empty'
+        else:
+            list_of_files = glob.glob(
+                os.path.join(pathTest, "data", "ori", '*'))  # * means all if need specific format then *.csv
+            latest_file = max(list_of_files, key=os.path.getctime)
+            print("glob latest_file:" + latest_file)
+
     elif pathToDir == 'changed':
         list_of_files = glob.glob(
             os.path.join(pathTest, "data", "changed", '*'))  # * means all if need specific format then *.csv
         latest_file = max(list_of_files, key=os.path.getctime)
         print("glob latest_file:" + latest_file)
+
     else:
         list_of_files = glob.glob(
             os.path.join(pathTest, "color_diffs", "Spring 2022", '*'))  # * means all if need specific format then *.csv
@@ -44,24 +49,15 @@ def latest_edited_file_glob(pathToDir):
 
 
 def read_csv(filename):
-    #pathTest = os.path.dirname(__file__)
-    #latest_edited_file = max([f for f in os.scandir(os.path.join(pathTest, "diffs", "Spring 2022"))], key=lambda x: x.stat().st_mtime).name
-    #print(latest_edited_file)
-    latest_edited_file_glob('test') # to get date
-
+    # latest_edited_file_glob('test') # to get date
     path = os.path.dirname(__file__)
     print('File name :    ', os.path.basename(__file__))
     print('Directory Name:     ', path)
 
-    # titles_df = pd.read_csv(r'C:\Users\ACER\PycharmProjects\diff_calculator\data\header\header.CSV', header=0)
-    # print(titles_df.shape)
-
     if filename == 'ori':
         #latest_edited_file = max([f for f in os.scandir(os.path.join(path, "data", "ori"))], key=lambda x: x.stat().st_mtime).name
         #pathFile = os.path.join(path, "data", "ori", latest_edited_file)
-
         pathFile = latest_edited_file_glob('ori')
-        #print(latest_edited_file)
 
         try:
             modified = os.path.getmtime(pathFile)
@@ -73,14 +69,9 @@ def read_csv(filename):
             file_date = time.ctime(os.path.getmtime(pathFile))
 
     elif filename == 'new':
-
-        #latest_edited_file = max([f for f in os.scandir(os.path.join(path, "data", "changed"))], key=lambda x: x.stat().st_mtime).name
-        #pathFile = os.path.join(path, "data", "changed", latest_edited_file)
-
         pathFile = latest_edited_file_glob('changed')
-
-        #print(latest_edited_file)
-
+        if pathFile == 'empty':
+            print('')
         try:
             modified = os.path.getmtime(pathFile)
             year, month, day, hour = time.localtime(modified)[:-5]
@@ -91,39 +82,23 @@ def read_csv(filename):
         except:
             file_date = time.ctime(os.path.getmtime(pathFile))
         print('pathFile is:', pathFile)
-        print(pathFile)
 
-    #df = pd.read_csv(pathFile, header=None)
     df = pd.read_csv(pathFile, header=None)
-    # df1.columns = df.columns
-    # print(df.iloc[0, 66])
-    # print(df)
 
     return df, file_date
 
-
 def duplicate_entry_merger(df):
-    # df.to_excel('diff_out_original.xlsx')
     df = df.applymap(str)
     df = df.groupby([df.columns[0], df.columns[7]]).agg(" ; ".join).reset_index()
-    # df.to_excel('diff_out.xlsx')
     return df
 
 
 if __name__ == '__main__':
-
+    # pathFile = latest_edited_file_glob('changed')
     today = date.today()
 
-    df_original_file, ori_date = read_csv('ori')
-    df_modified_file, new_date = read_csv('new')
-
-    # filtering out the dfs for CHEM
-    df_original_file = df_original_file.loc[df_original_file.iloc[:, 14] == "CHEM"]
-    df_modified_file = df_modified_file.loc[df_modified_file.iloc[:, 14] == "CHEM"]
-
-    for i in range(df_original_file.shape[0]):
-        for j in range(df_original_file.shape[1]):
-            df_original_file.iloc[i, j] = format_number(df_original_file.iloc[i, j])
+    df_modified_input_file, new_date = read_csv('new')
+    df_modified_file = df_modified_input_file.loc[df_modified_input_file.iloc[:, 14] == "CHEM"]
 
     for i in range(df_modified_file.shape[0]):
         for j in range(df_modified_file.shape[1]):
@@ -132,13 +107,65 @@ if __name__ == '__main__':
     filtered_columns = [0, 4, 7, 15, 16, 17, 18, 19, 21, 23, 25, 27, 34, 35, 36, 38, 51, 53, 57, 66, 67, 81, 82, 83, 90,
                         91,
                         97, 110, 112]
+    df_filtered_mod = df_modified_file.iloc[:]
+
+    if len(df_modified_file.columns) > 140:  # avoids applying filters to short files
+        df_filtered_mod = df_modified_file.iloc[:, filtered_columns]
+
+
+    modified_term = df_filtered_mod.iloc[0, 1]
+    print('modified_term is:', modified_term)
+
+    pathTest = os.path.dirname(__file__)
+    pathFile = latest_edited_file_glob('ori')
+    if pathFile == 'empty':
+        pathFile = os.path.join(pathTest, "color_diffs", f"{modified_term}")
+        list_of_files = glob.glob(
+            os.path.join(pathFile, '*.csv'))  # * means all if need specific format then *.csv
+        latest_file = max(list_of_files, key=os.path.getctime)
+        pathFile = latest_file
+        print("glob latest_file:" + latest_file)
+        # modified = os.path.getmtime(pathFile)
+        # year, month, day, hour = time.localtime(modified)[:-5]
+        # ori_date = str(str(year) + '_' + str(month) + '_' + str(day))
+
+    # ori_file_path = os.path.join(pathTest, 'data', 'ori', 'CLASSCHED.csv')
+    # if not os.path.isfile(ori_file_path):
+    #     print('ori is empty. assume you want to compare to most recent colored diff')
+    #     pathFile = os.path.join(pathTest, "color_diffs", f"{modified_term}", '*')
+        modified = os.path.getmtime(pathFile)
+        year, month, day, hour = time.localtime(modified)[:-5]
+        ori_date = str(str(year) + '_' + str(month) + '_' + str(day))
+        # ori_date = time.ctime(os.path.getmtime(pathFile))
+    else:
+        modified = os.path.getmtime(pathFile)
+        year, month, day, hour = time.localtime(modified)[:-5]
+        ori_date = str(str(year) + '_' + str(month) + '_' + str(day))
+        print("Original Date:", ori_date)
+        print('pathFile is:', pathFile)
+
+    df_original_file = pd.read_csv(pathFile, header=None)
+
+    # df_original_file, ori_date = read_csv('ori')
+    df_original_file = df_original_file.loc[df_original_file.iloc[:, 14] == "CHEM"]
+
+    for i in range(df_original_file.shape[0]):
+        for j in range(df_original_file.shape[1]):
+            df_original_file.iloc[i, j] = format_number(df_original_file.iloc[i, j])
+
 
     df_filtered_ori = df_original_file.iloc[:]
-    df_filtered_mod = df_modified_file.iloc[:]
+
     if len(df_modified_file.columns) > 140:  # avoids applying filters to short files
-        # print(len(df_modified_file.columns),'len(df_modified_file.columns)')
-        df_filtered_ori = df_original_file.iloc[:, filtered_columns]
-        df_filtered_mod = df_modified_file.iloc[:, filtered_columns]
+       df_filtered_mod = df_modified_file.iloc[:, filtered_columns]
+
+
+    print('83 ', df_filtered_mod[83])
+    df_instructor = df_filtered_mod.iloc[:,[3,6,8,17,23]]
+    print('df_instructor ', df_instructor)
+    print(df_instructor.shape)
+    # ff = pd.pivot_table(df_instructor, values=['83'], index=['53', '83'], columns=['53'], aggfunc=np.sum, fill_value=0)
+
 
     term = df_filtered_mod.iloc[0, 1]
     print('term' + term)
@@ -149,24 +176,20 @@ if __name__ == '__main__':
     df_filtered_ori = duplicate_entry_merger(df_filtered_ori)
     df_filtered_mod = duplicate_entry_merger(df_filtered_mod)
     # ----------------
+    df_modified_input_file.to_csv(f'color_diffs/{modified_term}/{new_date}_full_out.csv', sep=',', index=False, header=False)
 
     df_changes = pd.concat([df_filtered_ori.assign(type='original'), df_filtered_mod.assign(type='modified')])
     df_changes = df_changes.drop_duplicates(keep=False, subset=df_changes.columns.difference(['type']))
 
-    df_changes.to_csv(f'data/diffs/{ori_date}_{new_date}_diff_out.csv', sep=',', index=False, header=False)
-    # df_changes.to_csv('diff_out.csv', sep=',', index=False, header=False)
+    df_modified_file.to_csv(f'color_diffs/{modified_term}/{ori_date}_{new_date}_diff_out.csv', sep=',', index=False, header=False)
 
-    # df_changes_arranged = pd.read_csv('diff_out.csv', header=None)
     df_changes_arranged = df_changes
 
     df_changes_arranged = df_changes_arranged.sort_values(
         by=[df_changes_arranged.columns[0], df_changes_arranged.columns[7]], ascending=True)
-    df_changes_arranged.to_csv(f'data/diffs/{ori_date}_{new_date}_diff_out_arranged.csv', sep=',', index=False,
-                               header=False)
+    # df_changes_arranged.to_csv(f'color_diffs/{modified_term}/{ori_date}_{new_date}_diff_out_arranged.csv', sep=',', index=False,
+    #                            header=False)
 
-    # print(df_changes_arranged.shape)
-    # print(df_changes_arranged.shape[0])
-    # print(df_changes_arranged.shape[1])
 
     j = 0
     i = 0
@@ -223,9 +246,6 @@ if __name__ == '__main__':
 
     fill_pattern_from_original = PatternFill(patternType='solid', fgColor='FFA500')
     fill_pattern_from_modified = PatternFill(patternType='solid', fgColor='66CDAA')
-
-    # print('data/////')
-    # print(ws.cell(1,30).value)
 
     for l in range(len(df_changes_arranged)):
         if ws.cell(l + 1, 30).value == "original":
